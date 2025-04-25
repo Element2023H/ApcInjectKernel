7cf1e94fa9061cc8effac00eb41c02a98ee2429f
ellen2015 <772205152@qq.com>
2025-04-25 23:26:21 +0800

    add loglib

--- TestApcInject/main.cpp
+++ TestApcInject/main.cpp
@@ -1,6 +1,10 @@
 #include "../ApcInject.h"
+#include "Log.hpp"
 
 UNICODE_STRING g_InjectDll;
+UNICODE_STRING g_InjectDll32;
+
+HANDLE g_hFile{ nullptr };
 
 EXTERN_C
 {
@@ -24,18 +28,21 @@ PloadImageNotifyRoutine(
 	{
 		return;
 	}
+
 	NTSTATUS status{ STATUS_SUCCESS };
 	PUNICODE_STRING pProcessImage{ nullptr };
 	PEPROCESS pProcess{ nullptr };
 	status = PsLookupProcessByProcessId(ProcessId, &pProcess);
 	if (!NT_SUCCESS(status))
 	{
+		LOGERROR(status, "PsLookupProcessByProcessId failed. ProcessId: %d", HandleToULong(ProcessId));
 		return;
 	}
 
 	status = SeLocateProcessImageName(pProcess, &pProcessImage);
 	if (!NT_SUCCESS(status))
 	{
+		LOGERROR(status, "SeLocateProcessImageName failed. ProcessId: %d", HandleToULong(ProcessId));
 		ObDereferenceObject(pProcess);
 		return;
 	}
@@ -49,6 +56,12 @@ PloadImageNotifyRoutine(
 			//DbgBreakPoint();
 			ApcInjectNativeProcess(FullImageName, ProcessId, ImageInfo, &g_InjectDll);
 		}
+		else if (KWstrnstr(pProcessImage->Buffer, L"SysWOW64\\notepad.exe") &&
+			KWstrnstr(FullImageName->Buffer, L"SysWOW64\\ntdll.dll"))
+		{
+			//DbgBreakPoint();
+			ApcInjectWow64Process(FullImageName, ProcessId, ImageInfo, &g_InjectDll32);
+		}
 
 		ExFreePool(pProcessImage);
 		ObDereferenceObject(pProcess);
@@ -60,6 +73,11 @@ VOID
 DriverUnload(PDRIVER_OBJECT DriverObject)
 {
 	UNREFERENCED_PARAMETER(DriverObject);
+	if (g_hFile)
+	{
+		ZwClose(g_hFile);
+		g_hFile = nullptr;
+	}
 	PsRemoveLoadImageNotifyRoutine(reinterpret_cast<PLOAD_IMAGE_NOTIFY_ROUTINE>(PloadImageNotifyRoutine));
 }
 
@@ -75,10 +93,17 @@ DriverEntry(
 	ExInitializeDriverRuntime(DrvRtPoolNxOptIn);
 
 	RtlInitUnicodeString(&g_InjectDll, L"C:\\InjectDir\\InjectDll_x64.dll");
+	RtlInitUnicodeString(&g_InjectDll32, L"C:\\InjectDir\\InjectDll_x86.dll");
+
 
 	
 	DriverObject->DriverUnload = DriverUnload;
 
+	status = InitializeLogFile(L"\\??\\C:\\desktop\\Log.txt");
+	if (!NT_SUCCESS(status))
+	{
+		DbgPrint("status = %08X", status);
+	}
 
 	status = PsSetLoadImageNotifyRoutine(reinterpret_cast<PLOAD_IMAGE_NOTIFY_ROUTINE>(PloadImageNotifyRoutine));
 
